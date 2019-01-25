e03497e8580d318998816e9ea6bfde3ec6c8dedf
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-01-25 16:11:25 +0900

    fix bug of forward/backward prob

--- hmm.py
+++ hmm.py
@@ -1,6 +1,13 @@
 import numpy as np
 import argparse
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+#np.set_printoptions(formatter={'float': '{: 0.3e}'.format}, linewidth=200)
+np.set_printoptions(suppress=True)
+
 """
 HMM module
 - viterbi algorithm for optimal path
@@ -46,23 +53,31 @@ def get_emission_probability(i, j, prob, k=2):
                 p += prob[ (i-1+x)*2 + layout[x], (i-1+y)*2 + layout[y] ]
         return p
     
-def show_benchmark(layout, P, k, head=10):
+def show_benchmark(layout, pro, k, head=10):
     """debug用のbenchmark関数"""
+    #P = np.exp(pro)
+    P = pro
     N = 0
+    print(layout)
     for i in range(len(layout)):
         N += 1
         if N == head:
-            break
+            continue
         if layout[i] == 0:
             #continue
             pass
-        print(layout[i], end=' ')
+        
+        # layout 表示部分
+        print(i, layout[i], end=' ')
+        
         for j in range(len(P[i])):
             if j == np.argmax(P[i]):
-                print('\x1b[31m{:.2e}\x1b[0m'.format(P[i][j]), end=' ')
+                print('\x1b[31m{:.7e}\x1b[0m'.format(P[i][j]), end=' ')
             else:
-                print('{:.2e}'.format(P[i][j]), end=' ')
+                print('{:.7e}'.format(P[i][j]), end=' ')
+        
         print(end='\n')
+        print(np.exp(P[i+1][0:2**(k-1)]).sum(), np.exp(P[i+1][2**(k-1):2**k]).sum())
 
 def run_hmm(prob, k=4, n=5000, debug=False):
     """
@@ -134,14 +149,18 @@ def run_hmm(prob, k=4, n=5000, debug=False):
     #print(A)
     for i in range(1, n-k+2):
         for j in range(ns):
-            S = 0
             tmp = F[i-1] + A[:,j]
-            #print(i,j,np.exp(tmp))
             #print(i, j, np.exp(F[i-1] + A[:,j]), np.exp(F[i-1]), np.exp(A[:,j]))
-            for l in range(ns):
-                S = log_add(S, tmp[l])
+            tmp2 = tmp[np.where(tmp != -np.inf)]
+            if len(tmp2) > 0:
+                S = tmp2[0]
+                for l in range(1, len(tmp2)):
+                    S = log_add(S, tmp2[l])
+            else:
+                S = -np.inf
+            #print(i, j, np.exp(S))
             F[i][j] = get_emission_probability(i, j, prob, k) + S
-    #print('F', F)
+            #print(np.exp(F[i][j]))
     
     # 後ろ向きアルゴリズム
     B = np.zeros((n-k+2, ns)) 
@@ -149,18 +168,27 @@ def run_hmm(prob, k=4, n=5000, debug=False):
     for i in range(n-k, -1, -1):
         #print('b', i)
         for j in range(ns):
-            S = 0
-            tmp = B[i+1] + A[j]
+            S = -np.inf
+            trans = A[j]
+            tmp = B[i+1] + trans
+            #trans[-1] = -np.inf
+            #print('A', np.exp(trans))
             #print(i, j, np.exp(tmp))
             for l in range(ns):
+                e = get_emission_probability(i+1, l, prob, k)
+                #print(l, np.exp(e))
                 #print(i, j, l, np.exp(tmp[l] + get_emission_probability(i+1, l, prob, k)))
-                S = log_add(S, tmp[l] + get_emission_probability(i+1, l, prob, k))
+                S = log_add(S, tmp[l] + e)
             B[i][j] = S
-    #print(B)
     
     P = F+B  # P[t][j] = 時刻tにjのステートにいる確率
     
-    #print(np.exp(F+B))
+    if debug:
+        print(np.exp(state))
+        print(np.exp(F))
+        print(np.exp(B))
+        print(np.exp(A))
+        print(np.exp(P))
     
     return orientation, P
 
@@ -169,7 +197,14 @@ def log_add(lA, lB):
     """
     suppose lA = log(A), lB = log(B) then return log(A+B)
     """
-    return max(lA, lB) + np.log(np.exp(lA - max(lA, lB)) + np.exp(lB - max(lA, lB)))
+    if lA == -np.inf and lB == -np.inf:
+        return -np.inf
+    elif lA == -np.inf:
+        return lB
+    elif lB == -np.inf:
+        return lA
+    else:
+        return max(lA, lB) + np.log(np.exp(lA - max(lA, lB)) + np.exp(lB - max(lA, lB)))
 
 if __name__ == '__main__':
     psr = argparse.ArgumentParser()
@@ -180,10 +215,13 @@ if __name__ == '__main__':
     args = psr.parse_args()
     
     if args.debug:
-        _ = run_hmm(None, k=args.k, n=args.n, debug=True)
+        layout, P = run_hmm(None, k=args.k, n=args.n, debug=True)
+        show_benchmark(layout, P, k=args.k)
+        
     elif args.prob:
         print(args.prob, args.k, args.n)
         prob = np.load(args.prob)
-        _ = run_hmm(prob, k=args.k, n=args.n)
+        layout, P = run_hmm(prob, k=args.k, n=args.n)
+        show_benchmark(layout, P, k=args.k)
     else:
         print('you should select either --prob prob.npy or --debug option to run hmm.')
