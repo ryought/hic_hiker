c622c76c5ac70f11dddb3bcf83a458ae02ff1650
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-09-11 19:15:34 +0900

    pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,28 @@ from setuptools import setup
 
 setup(
         name="hic_hiker",
-        version="0.0.1",
-        description="",
+        version="1.0.0",
+        description="3D-DNA scaffolds refinement",
         long_description=open('README.md').read(),
         long_description_content_type="text/markdown",
-        url="https://github.com/ryought/HiC-Hiker",
+        url="https://github.com/ryought/hic_hiker",
         author="ryought",
         author_email="ryonakabayashi@gmail.com",
         license="MIT",
         packages=[
             "hic_hiker",
+            ],
+        install_requires=[
+            "numpy",
+            "matplotlib",
+            "scikit-learn",
+            "scipy",
+            "pandas",
+            "feather-format",
+            "biopython",
+            "pysam",
+            "tqdm",
+            "matplotlib-scalebar"
             ],
         entry_points={
             "console_scripts": [
